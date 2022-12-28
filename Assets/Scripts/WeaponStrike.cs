e4ff61a2e79f4a2c9bacf797754abd31d2e4b0f7
kubatokarski <kuba.tokarski@gmail.com>
2022-12-28 23:28:54 +0100

    Attack area modified.

--- Assets/Scripts/WeaponStrike.cs
+++ Assets/Scripts/WeaponStrike.cs
@@ -12,8 +12,10 @@ public class WeaponStrike : MonoBehaviour
     [SerializeField] GameObject WhipAttackR;
 
     PlayerMovement playerMovement;
-    [SerializeField] Vector2 whipAttackSize = new Vector2(2.1f, 0.7f);
+    [SerializeField] Vector2 whipAttackSize = new Vector2(2f, 0.7f);
     [SerializeField] int axeDamage = 1;
+
+    LineRenderer l;
 
     private void Awake ()
     {
@@ -32,16 +34,27 @@ public class WeaponStrike : MonoBehaviour
 
     private void Attack()
     {
+        //float xAttackMultiplier = 0.9f;
+        //float yAttackMultiplier = 0.9f;
+
         timer = timeToAttack;
 
         if( playerMovement.lastHorizontalVector > 0) {
             WhipAttackR.SetActive(true);
             Collider2D[] colliders = Physics2D.OverlapBoxAll(WhipAttackR.transform.position, whipAttackSize, 0f);
             ApplyDamage(colliders);
+            //DrawDebugLine(  new Vector3(WhipAttackR.transform.position.x, WhipAttackR.transform.position.y),
+            //                new Vector3(WhipAttackR.transform.position.x + WhipAttackR.GetComponent<SpriteRenderer>().bounds.size.x * xAttackMultiplier, WhipAttackR.transform.position.y + WhipAttackR.GetComponent<SpriteRenderer>().bounds.size.y));
+            //Debug.Log( "attack width: "  + (WhipAttackR.GetComponent<SpriteRenderer>().bounds.size.x * xAttackMultiplier) + 
+            //           "attack height: " + (WhipAttackR.GetComponent<SpriteRenderer>().bounds.size.y * yAttackMultiplier) );
         } else {
             WhipAttackL.SetActive(true);
             Collider2D[] colliders = Physics2D.OverlapBoxAll(WhipAttackL.transform.position, whipAttackSize, 0f);
             ApplyDamage(colliders);
+            //DrawDebugLine(  new Vector3(WhipAttackL.transform.position.x, WhipAttackL.transform.position.y),
+            //                new Vector3(WhipAttackL.transform.position.x - WhipAttackL.GetComponent<SpriteRenderer>().bounds.size.x * xAttackMultiplier, WhipAttackL.transform.position.y + WhipAttackL.GetComponent<SpriteRenderer>().bounds.size.y));
+            //Debug.Log( "attack width: "  + (WhipAttackL.GetComponent<SpriteRenderer>().bounds.size.x * xAttackMultiplier) + 
+            //           "attack height: " + (WhipAttackL.GetComponent<SpriteRenderer>().bounds.size.y * yAttackMultiplier) );                    
         }
     }
 
@@ -55,5 +68,19 @@ public class WeaponStrike : MonoBehaviour
                 colliders[i].GetComponent<Enemy>().TakeDamage(axeDamage);
             }
         }
+    }
+
+    private void DrawDebugLine(Vector3 startPoint, Vector3 endPoint) {
+        if (l==null) {
+            l = gameObject.AddComponent<LineRenderer>();
+        }
+        
+        List<Vector3> pos = new List<Vector3>();
+        pos.Add(startPoint);
+        pos.Add(endPoint);
+        l.startWidth = 0.01f;
+        l.endWidth = 0.01f;
+        l.SetPositions(pos.ToArray());
+        l.useWorldSpace = true;
     }
 }
