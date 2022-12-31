dc0e9a29cb6343b8abecf2c0b29ba8ebeb020ccd
kubatokarski <kuba.tokarski@gmail.com>
2022-12-31 22:36:57 +0100

    Initial Enemy spawn.

--- Assets/Scripts/EnemySpawnerV2.cs
+++ Assets/Scripts/EnemySpawnerV2.cs
@@ -6,6 +6,7 @@ public class EnemySpawnerV2 : MonoBehaviour
 {
     [SerializeField] private GameObject swarmerPrefab;
     [SerializeField] private GameObject bigSwarmerPrefab;
+    private int initialSpawnAmount = 3;
 
     [SerializeField] private float swarmerInterval = 3.5f;
     [SerializeField] private float bigSwarmerInterval = 10f;
@@ -15,13 +16,51 @@ public class EnemySpawnerV2 : MonoBehaviour
     void Start()
     {
         StartCoroutine(spawnEnemy(swarmerInterval, swarmerPrefab));
-        StartCoroutine(spawnEnemy(bigSwarmerInterval, bigSwarmerPrefab));
+        for(int i=0; i<initialSpawnAmount; i++)
+        {
+            spawnInitialEnemy(swarmerPrefab);
+        }
+        //StartCoroutine(spawnEnemy(bigSwarmerInterval, bigSwarmerPrefab));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 4f), 0), Quaternion.identity);
+        newEnemy.transform.parent = this.transform;
         StartCoroutine(spawnEnemy(interval, enemy));
     }
+
+    private void spawnInitialEnemy(GameObject enemy) {
+        float safeDistance = 3f;
+        float maxX = 8f;
+        float maxY = 4.5f;
+
+        Vector2 spawnPosition = RandomRangeClamped(maxX, maxY, safeDistance);
+
+        GameObject newEnemy = Instantiate(enemy, new Vector3(spawnPosition.x, spawnPosition.y, 0), Quaternion.identity);
+        newEnemy.transform.parent = this.transform;
+    }
+
+    private Vector2 RandomRangeClamped(float maxX, float maxY, float min)
+    {
+        Vector2 result = new Vector2();
+        result.x = Random.Range(-maxX, maxX);
+        result.y = Random.Range(-maxY, maxY);
+        if(result.magnitude < min)
+        {
+            float modifyXorY = Random.value;
+
+            if (modifyXorY > 0.5)
+            {
+                result.x += Mathf.Sign(result.x) * min;
+            } else
+            {
+                result.y += Mathf.Sign(result.y) * min;
+            }
+        }
+        return result;
+
+    }
+
 }
