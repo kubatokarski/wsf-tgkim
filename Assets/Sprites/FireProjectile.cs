0e47533fa5a3d902e9083043fb9a196a6c5d496c
kubatokarski <kuba.tokarski@gmail.com>
2023-01-10 18:24:02 +0100

    Basic projectiles movement.

--- Assets/Sprites/FireProjectile.cs
+++ Assets/Sprites/FireProjectile.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class FireProjectile : MonoBehaviour
 {
-    private int counter = 0;
-    private bool isMoving = false;
-    private float speed = 0.05f;
     private Vector3 direction;
+    private float speed = 0.01f;
+    
 
     private void Start()
     {
@@ -15,6 +14,12 @@ public class FireProjectile : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //this.transform.position += direction * speed;
+        this.transform.position += direction * 0.05f;
+        //this.transform.position += new Vector3(0.05f, 0.05f);
+    }
+
+    public void setDirection (Vector3 v)
+    {
+        direction = v;
     }
 }
--- Assets/Sprites/ProjectileSpawner.cs
+++ Assets/Sprites/ProjectileSpawner.cs
@@ -7,27 +7,58 @@ public class ProjectileSpawner : MonoBehaviour
     private float fireProjectileInterval = 1;
     private GameObject playerCharacter;
     [SerializeField] private GameObject fireProjectile;
+    [SerializeField] GameObject enemies;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         StartCoroutine( spawnProjectile( fireProjectileInterval, new Vector3(0f, 0f), new Vector3 (1f, 1f)) );
         playerCharacter = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
 
+
     private IEnumerator spawnProjectile( float interval, Vector3 spawnPosition, Vector3 direction )
     {
+        Transform closestEnemy;
+        int enemiesCount = enemies.transform.childCount;
+        if (enemiesCount > 0)
+        {
+            Debug.Log("Finding closest enemy...");
+            float minDistance = Vector2.Distance(this.transform.position, enemies.transform.GetChild(0).transform.position);
+            int closestEnemyIndex = 0;
+
+            for (var i = 1; i < enemiesCount; i++)
+            {
+                float dist = Vector2.Distance(this.transform.position, enemies.transform.GetChild(i).transform.position);
+                if (dist < minDistance)
+                {
+                    closestEnemyIndex = i;
+                    minDistance = dist;
+                }
+            }
+
+            closestEnemy = enemies.transform.GetChild(closestEnemyIndex);
+
+            Color color;
+            ColorUtility.TryParseHtmlString("#4CFFF6", out color);
+            enemies.transform.GetChild(closestEnemyIndex).GetComponent<SpriteRenderer>().color = color;
+
+            Vector3 closestEnemyDirection = (closestEnemy.position - this.transform.position).normalized;
+        }
+
         yield return new WaitForSeconds(interval);
         Debug.Log("spawning projectile");
         GameObject newProjectile = Instantiate(fireProjectile, playerCharacter.transform.position, Quaternion.identity);
         newProjectile.transform.parent = this.transform;
-
+        FireProjectile newFireProjectile = newProjectile.GetComponent<FireProjectile>();
+        newFireProjectile.setDirection(new Vector3(1f, 1f, 0) );
+                
         StartCoroutine( spawnProjectile(interval, spawnPosition, direction) );
     }
 }
