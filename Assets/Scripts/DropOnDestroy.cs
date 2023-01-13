e93ab541cf785766a22b563a1ffd2ddb39d807f4
kubatokarski <kuba.tokarski@gmail.com>
2023-01-13 18:58:07 +0100

    OnDestroy instantiate error fixed.

--- Assets/Scripts/DropOnDestroy.cs
+++ Assets/Scripts/DropOnDestroy.cs
@@ -7,12 +7,25 @@ public class DropOnDestroy : MonoBehaviour
     [SerializeField] GameObject healthPickup;
     [SerializeField] [Range(0f, 1f)] float chanceToSpawn = 1f;
 
+    private bool isQuitting = false;
+
+    private void OnApplicationQuit()
+    {
+        isQuitting = true;        
+    }
+
     private void OnDestroy()
     {
-        if(Random.value < chanceToSpawn)
+        if (!isQuitting)
         {
-            Transform t = Instantiate(healthPickup).transform;
-            t.position = transform.position;
+            if(Random.value < chanceToSpawn)
+            {
+                //Transform t = Instantiate(healthPickup).transform;
+                //t.position = transform.position;
+                GameObject newPickup = Instantiate(healthPickup);
+                newPickup.transform.position = transform.position;
+
+            }
         }
     }
 }
